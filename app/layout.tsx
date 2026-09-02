ccc1a4abd7f31b538414759614fea153ffe07557
SibusisoTAU <mtausibusiso@gmail.com>
2026-09-02 19:31:47 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
-export default function RootLayout({children}: {children: React.ReactNode}) {
-  return <html lang="en"><body>{children}</body></html>
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body style={{margin:0}}>{children}</body>
+    </html>
+  )
 }
